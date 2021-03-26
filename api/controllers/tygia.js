fbcd6d766495353fc62648e24c9bdb368acf7369
Tuong Phan <tramphongsts@gmail.com>
2021-03-26 13:41:35 +0700

    change date query from 03/03/2021 to 03032021

--- api/controllers/tygia.js
+++ api/controllers/tygia.js
@@ -32,9 +32,10 @@ async function crawlTyGiaVietcombank(req, res) {
     URL =
       "https://portal.vietcombank.com.vn/Usercontrols/TVPortal.TyGia/pXML.aspx";
   } else {
+    var dateWithSlash = date.slice(0,2) + "/" + date.slice(2,4) + "/" +date.slice(4,8);
     URL =
       "https://portal.vietcombank.com.vn/UserControls/TVPortal.TyGia/pListTyGia.aspx?BacrhID=68&isEn=True&txttungay=" +
-      date;
+      dateWithSlash;
   }
   var result;
   const options = {
